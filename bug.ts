5ad59c42aed109c0b28fe6768be008cd906b229c
BugHunter-X <askzenai@gmail.com>
2025-01-02 23:02:07 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,25 @@
+function printArray(arr: any[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+const myArray = [1, 2, 3, 4, 5];
+printArray(myArray); // Works fine
+
+const myStringArray = ['a', 'b', 'c', 'd', 'e'];
+printArray(myStringArray); // Works fine
+
+const myMixedArray = [1, 'a', 2, 'b', 3];
+printArray(myMixedArray); // Works fine
+
+const myObjectArray = [{id:1, name: "Test"}, {id:2, name: "Test2"}]
+printArray(myObjectArray); // This will print [object Object] for each object
+
+//If we want to print the content of the objects we should use the following code
+function printObjectArray(arr: any[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(JSON.stringify(arr[i]));
+  }
+}
+printObjectArray(myObjectArray);
